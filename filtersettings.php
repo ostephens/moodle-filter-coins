59ab4eba840804026c2737548bdfdbf6bd9d185b
Owen Stephens <owen@ostephens.com>
2012-04-18 18:37:53 +0100

    Correct admin setting for alttext

--- filtersettings.php
+++ filtersettings.php
@@ -6,13 +6,13 @@ if ($ADMIN->fulltree) {
 
 	$settings->add(new admin_setting_configtext('filter_coins_baseurl',
         get_string('baseurl', 'filter_coins'),
-        get_string('configbaseurl', 'filter_coins'), '', PARAM_NOTAGS));
+        get_string('configbaseurl', 'filter_coins'), 'http://worldcatlibraries.org/registry/gateway', PARAM_NOTAGS));
 
 	$settings->add(new admin_setting_configtext('filter_coins_linkicon',
         get_string('linkicon', 'filter_coins'),
         get_string('configlinkicon', 'filter_coins'), '', PARAM_NOTAGS));
 
-	$settings->add(new admin_setting_configtext('filter_coins_linkicon',
+	$settings->add(new admin_setting_configtext('filter_coins_linkalttext',
         get_string('linkalttext', 'filter_coins'),
         get_string('configlinkalttext', 'filter_coins'), '', PARAM_NOTAGS));
 }
