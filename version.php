90be16d8a8af3df013e3f70820d361664d8d2b44
Owen Stephens <owen@ostephens.com>
2012-04-21 15:04:11 +0100

    corrected version number

--- version.php
+++ version.php
@@ -24,7 +24,6 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-
 $plugin->version   = 2012041100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011112900;        // Requires this Moodle version
+$plugin->requires  = 2011120100;        // Requires this Moodle version
 $plugin->component = 'filter_coins';  // Full name of the plugin (used for diagnostics)
